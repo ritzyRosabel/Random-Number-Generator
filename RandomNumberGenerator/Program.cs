829c56b73e020c7d7602ac7390f50b9ccbd9b856
opemipo.olugbenga@fcmb.com <rosabelolugbenga@yahoo.com>
2020-01-31 17:32:52 +0100

    Add project files.

---
+++ RandomNumberGenerator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomNumberGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                var randomNumber = new Random().Next(999999999).ToString() + new Random().Next(99999).ToString();
+                Console.WriteLine(randomNumber);
+        }
+    }
+}
